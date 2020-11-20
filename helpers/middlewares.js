4c7a8029f7db947cb974ca62cb0d52f4079543f4
samsmc <samantha.martins@gmail.com>
2020-11-20 10:47:13 +0100

    login works

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -12,6 +12,7 @@ exports.isNotLoggedIn = () => (req, res, next) => {
 
 exports.validationLoggin = () => (req, res, next) => {
   const { email, password } = req.body;
+  console.log("req.body", req.body)
 
   if (!email || !password) next(createError(400));
   else next();
