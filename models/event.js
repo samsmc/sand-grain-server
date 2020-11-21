1bbbb68796d964646b6930cdbe99fe4f241e8359
samsmc <samantha.martins@gmail.com>
2020-11-22 00:04:46 +0100

    update saturday

--- models/event.js
+++ models/event.js
@@ -3,14 +3,14 @@ const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
   name: { type: String, required: true },
-  img: { type: String, required: true },
+  img: { type: String},
   description: { type: String, required: true },
   participantsLimit: Number,
   participants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   organizer: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   location: { type: String, required: true },
   date: { type: Date, required: true },
-  time: { type: Date, required: true },
+  time: { type: String, required: true },
   category: { type: String, required: true },
   stars: { type: Number, required: true }
 });
