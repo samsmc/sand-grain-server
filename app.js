59a06b36f4b2d22e629e3db4f133dcc7d9865cb1
roicid <roi_cid89@hotmail.com>
2020-11-25 16:33:14 +0100

    heroku

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN],
+    origin: [process.env.PUBLIC_DOMAIN, 'https://sand-grain.herokuapp.com'],
   })
 );
 // app.use((req, res, next) => {
@@ -74,6 +74,13 @@ app.use("/user", usersRouter);
 app.use("/", indexRouter);
 app.use("/auth", auth);
 app.use("/", uploadRouter);
+
+// ROUTE FOR SERVING REACT APP (index.html)
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 
 // ERROR HANDLING
 // catch 404 and forward to error handler
---
+++ public/precache-manifest.fe7962455333b3ed5b44006f5b9d4c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bfadfcb5f044ae2bf64aca50c0d92a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f1b315216a7c9509e9d",
+    "url": "/static/css/main.0113ae91.chunk.css"
+  },
+  {
+    "revision": "9713022bb9a6a92ca6e6",
+    "url": "/static/js/2.8898973e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8898973e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1b315216a7c9509e9d",
+    "url": "/static/js/main.c8152c02.chunk.js"
+  },
+  {
+    "revision": "5179ff928fc7dafdbfe9",
+    "url": "/static/js/runtime-main.81fc7dff.js"
+  }
+]);
